984e07ab9d6d374514a0d08be13d57b9fd479b75
Sneus <SneusGeek@gmail.com>
2024-02-09 00:47:42 +0100

    Comitted

---
+++ app/Http/Requests/CreatePostRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreatePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'min:8'],
+            'slug' => ['required', 'min:8', 'regex:/^[0-9a-z\-]+$/'],
+            'content' => ['required']
+
+        ];
+    }
+
+    protected function prepareForValidation()
+    { this->merge(['slug'=> $this->input('slug') ?: \Str::slug($this->input('title'))]);
+    }
+}
