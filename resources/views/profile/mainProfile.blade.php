814c9f2c8735e553734eadae8a29993e6afcc44e
Mehdi <chihi.med@gmail.com>
2024-02-18 17:21:46 +0100

    fin css page profil avec images

--- resources/views/profile/mainProfile.blade.php
+++ resources/views/profile/mainProfile.blade.php
@@ -22,13 +22,13 @@
                     <h2 class="h2prof">Panel {{Auth::user()->rôle}}</h2>
                     <ul class="list-group">
 
-                        <li class="list-group-item li-prof"> <a href="{{route('auth.register')}}">Changer vos informations personnelles </a> </li>
-                        <li class="list-group-item li-prof"> <a href="{{route('blog.index')}}"> Choisir des articles à mettre dans votre panier de favoris</a> </li>
-                        <li class="list-group-item li-prof"> <a href="{{route('blog.search')}}"> Effectuer une recherche à l'aide de la barre de recherche sur l'icône loupe </a> </li>
+                        <li class="list-group-item li-prof"> <a href="{{route('auth.register')}}">Changer vos informations personnelles <img src="/img/user_black.svg"> </a> </li>
+                        <li class="list-group-item li-prof"> <a href="{{route('blog.index')}}"> Choisir des articles à mettre dans votre panier de favoris <img src="/img/star-filled.svg"></a> </li>
+                        <li class="list-group-item li-prof"> <a href="{{route('blog.search')}}"> Effectuer une recherche à l'aide de la barre de recherche sur l'icône <img src="/img/search_black.svg"> </a> </li>
                         @if (Auth::user()->rôle == "Admin")
-                            <li class="list-group-item li-prof"> <a href="{{route('profile.profile')}}"> Vous balader sur votre profil administrateur </a> </li>
-                            <li class="list-group-item li-prof"> <a href="{{route('blog.create')}}"> Créer de nouveaux articles </a> </li>
-                            <li class="list-group-item li-prof"> <a href="{{route('blog.create')}}"> Updater un article déjà existant </a> </li>
+                            <li class="list-group-item li-prof"> <a href="{{route('profile.profile')}}"> Vous balader sur votre profil administrateur <img src="/img/user_black.svg"> </a> </li>
+                            <li class="list-group-item li-prof"> <a href="{{route('blog.create')}}"> Créer de nouveaux articles <img src="/img/article_logo_black.svg"> </a> </li>
+                            <li class="list-group-item li-prof"> <a href="{{route('blog.create')}}"> Updater un article déjà existant <img src="/img/article_logo_black.svg"> </a> </li>
                         @endif
 
                     </ul>
