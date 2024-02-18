117be4917c7ecdef1001ccc4d2cb170b0dc3f38d
Mehdi <chihi.med@gmail.com>
2024-02-18 13:23:21 +0100

    chipotage CSS page profile

--- resources/views/profile/mainProfile.blade.php
+++ resources/views/profile/mainProfile.blade.php
@@ -18,13 +18,13 @@
                 <button class="btn-deco col-12">Se déconnecter</button>
             </form>
 
-                <div class="container mt-5">
-                    <h2>Panel {{Auth::user()->rôle}}</h2>
+                <div class="mt-3">
+                    <h2 class="h2prof">Panel {{Auth::user()->rôle}}</h2>
                     <ul class="list-group">
 
                         <li class="list-group-item">Changer vos informations personnelles</li>
-                        <li class="list-group-item">Choisir des articles à mettre à votre panier de favoris</li>
-                        <li class="list-group-item">Effectuer une recherche à l'aide de la barre de recherche se trouvant dans la navbar</li>
+                        <li class="list-group-item">Choisir des articles à mettre dans votre panier de favoris</li>
+                        <li class="list-group-item">Effectuer une recherche à l'aide de la barre de recherche sur l'icône loupe</li>
                         @if (Auth::user()->rôle == "Admin")
                             <li class="list-group-item">Vous balader sur votre profil administrateur</li>
                             <li class="list-group-item">Créer de nouveaux articles</li>
