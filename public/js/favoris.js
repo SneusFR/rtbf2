eefe964627e640700954ca017d7cd90fb4417ba2
Sneus <SneusGeek@gmail.com>
2024-04-05 13:53:23 +0200

    Plus rapide

--- public/js/favoris.js
+++ public/js/favoris.js
@@ -1,11 +1,23 @@
 
 $(document).ready(function(){
     console.log("Script chargé");
-    $('.bouton-circulaire-nofav').click(function(){
+    $('[class^="bouton"]').on('click', function(){
         // Récupérer l'URL d'ajout aux favoris et l'ID du post à partir de l'élément de bouton cliqué
         var ajouterFavUrl = $(this).data('ajouter-fav-url');
         var postId = $(this).data('post-id');
         var button = $(this);
+        var img = button.find('.star'); // Sélectionner l'élément img à l'intérieur du bouton
+
+
+        if (button.hasClass('bouton-circulaire-nofav')) {
+            button.removeClass('bouton-circulaire-nofav').addClass('bouton-circulaire-fav');
+            img.attr('src', '/img/star-filled.svg');
+        }
+
+        else {
+            button.removeClass('bouton-circulaire-fav').addClass('bouton-circulaire-nofav');
+            img.attr('src', '/img/star-empty.png');
+        }
 
         // Effectuer la requête AJAX
         $.ajax({
@@ -17,33 +29,7 @@ $(document).ready(function(){
             },
             success: function(data){
                 // Afficher un message de succès ou mettre à jour l'interface utilisateur
-                alert('Article ajouté aux favoris avec succès');
-                button.removeClass('bouton-circulaire-nofav').addClass('bouton-circulaire-fav');
-            },
-            error: function(xhr, status, error) {
-                alert('La requête AJAX a échoué : ' + error);
-            }
-        });
-    });
 
-    $('.bouton-circulaire-fav').click(function(){
-        // Récupérer l'URL d'ajout aux favoris et l'ID du post à partir de l'élément de bouton cliqué
-        var ajouterFavUrl = $(this).data('ajouter-fav-url');
-        var postId = $(this).data('post-id');
-        var button = $(this);
-
-        // Effectuer la requête AJAX
-        $.ajax({
-            type: 'POST',
-            url: ajouterFavUrl,
-            data: {
-                _token: $('meta[name="csrf-token"]').attr('content'),
-                post_id: postId
-            },
-            success: function(data){
-                // Afficher un message de succès ou mettre à jour l'interface utilisateur
-                alert('Article retiré des favoris avec succès');
-                button.removeClass('bouton-circulaire-fav').addClass('bouton-circulaire-nofav');
             },
             error: function(xhr, status, error) {
                 alert('La requête AJAX a échoué : ' + error);
