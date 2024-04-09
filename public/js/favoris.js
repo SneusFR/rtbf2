6a012a65dab3b39240e43c5790d7e0f87d45c148
Sneus <SneusGeek@gmail.com>
2024-04-09 15:06:11 +0200

    FINNNNNNNNNNNNNNNNNNNN

--- public/js/favoris.js
+++ public/js/favoris.js
@@ -2,58 +2,36 @@ $(document).ready(function(){
 
     gestionnaireClicBoutons();
 
+    var roleValue = $('main').data('role');
+
     // Gérer l'événement de clic sur les boutons
     // Définir une méthode pour gérer l'événement de clic sur les boutons
 
-    var roleValue = $('main').data('role');
-
     function gestionnaireClicBoutons() {
-        $('[class^="bouton-circulaire"]').on('click', function () {
-            var ajouterFavUrl = $(this).data('ajouter-fav-url');
-            var postId = $(this).data('post-id');
+        $('[class^="bouton-circulaire"]').off().on('click', function () {
             var button = $(this);
-            var img = button.find('.star');
-            var notif = $("#notification");
             var roleValue = $('main').data('role');
 
-
-            if (roleValue ==='invite') {
-                notif.removeClass('alert alert-success').addClass('alert alert-danger').html('Vous devez vous connecter');
-                notif.css('display', 'block');
-                setTimeout(function () {
-                    notif.css('display', 'none');
-                }, 3000); // 3000 millisecondes = 3 secondes
-
+            if (roleValue === 'invite') {
+                afficherNotification('alert-danger', 'Vous devez vous connecter');
                 return;
             }
-
-
-                // Intervertir class des boutons lors d'un clic sur ceux-ci
-            else if (button.hasClass('bouton-circulaire-nofav')) {
-                notif.removeClass('alert alert-danger').addClass('alert alert-success').html('Article ajouté aux favoris avec succès');
-                notif.css('display', 'block');
-                setTimeout(function () {
-                    notif.css('display', 'none');
-                }, 3000);
-
-            }
-
-            else if (button.hasClass('bouton-circulaire-fav')) {
-                notif.removeClass('alert alert-success').addClass('alert alert-danger').html('Article a bien été retiré des favoris');
-                notif.css('display', 'block');
-                setTimeout(function () {
-                    notif.css('display', 'none');
-                }, 3000); // 3000 millisecondes = 3 secondes
-            }
-
-            // Effectuer l'ajout ou le retrait du favori
+            afficherNotification(button.hasClass('bouton-circulaire-nofav') ? 'alert-success' : 'alert-danger', button.hasClass('bouton-circulaire-nofav') ? 'Article ajouté aux favoris avec succès' : 'Article a bien été retiré des favoris');
             toggleFavorite(button);
-
-            // Afficher les favoris après l'ajout ou le retrait
             displayFavorites();
         });
     };
 
+    //Gérer la notif qui s'affichera lors de la suppression ou du rajout d'un favoris
+    function afficherNotification(type, message) {
+        var notif = $("#notification");
+        notif.removeClass('alert alert-danger alert-success').addClass('alert ' + type).html(message).css('display', 'block');
+        setTimeout(function () {
+            notif.css('display', 'none');
+        }, 3000);
+    }
+
+    //Mettre un événément d'écoute on change sur les radio button
     $('input[type=radio][name=choice]').change(function() {
         if (this.value === 'form') {
             // Changer le comportement pour utiliser un formulaire
@@ -62,24 +40,20 @@ $(document).ready(function(){
             $('.bouton-circulaire-fav').off();
             $('.bouton-circulaire-nofav').off();
 
-            console.log('en mode form')
-        } else if (this.value === 'jquery') {
+        }
+        else if (this.value === 'jquery') {
             // Revenir au comportement jQuery/Ajax
             $('.bouton-circulaire-fav').prop('type', 'button');
             $('.bouton-circulaire-nofav').prop('type', 'button');
             gestionnaireClicBoutons();
-            console.log('en mode jquery ma poule')
         }
     });
 
     // Fonction pour gérer l'ajout ou le retrait d'un favori
     function toggleFavorite(button) {
-
         var removeAll = button.attr('data-removeAll');
         var retirerFavUrl = button.data('ajouter-fav-url');
         var postId = button.data('post-id');
-        var button = $('#' + postId);
-        var img = button.find('.star');
 
         $.ajax({
             type: 'POST',
@@ -89,28 +63,17 @@ $(document).ready(function(){
                 post_id: postId,
                 removeAll : removeAll
             },
-
             success: function (response){
-
-                if (removeAll === "true"){
-                    var notif = $("#notification");
-                    notif.removeClass('alert alert-success').addClass('alert alert-danger').html('Tous les articles ont été retirés de vos favoris');
-                    notif.css('display', 'block');
-                    setTimeout(function(){
-                        notif.css('display', 'none');
-                    }, 5000);
-
-                }
+                if (removeAll === "true") afficherNotification('alert-danger', 'Tous les articles ont été retirés de vos favoris');
             },
-
             error: function(xhr, status, error) {
                 alert('La requête AJAX a échoué : ' + error);
             }
         });
-
         displayFavorites();
     }
 
+    //Gérer l'affichage des favoris
     function displayFavorites() {
         $.ajax({
             type: 'GET',
@@ -130,20 +93,15 @@ $(document).ready(function(){
                 });
 
                 var testbouton = $('.bouton-circulaire-fav');
-
                 testbouton.removeClass('bouton-circulaire-fav').addClass('bouton-circulaire-nofav');
+                testbouton.find('.star').attr('src', '/img/star-empty.png');
 
-                var imgStars = testbouton.find('.star');
-
-                imgStars.each(function (index, star) {
-                    star.src = '/img/star-empty.png';
-                });
-
-
+                // Parcourir les favoris
                 $.each(favoritePosts, function(index, favoritePost) {
                     var postId = favoritePost.id_pos;
-                    button = $('#'+postId)
+                    var button = $('#' + postId); // Sélectionnez le bouton correspondant à l'ID du favori
                     var img = button.find('.star');
+
                     img.attr('src', '/img/star-filled.svg');
                     button.removeClass('bouton-circulaire-nofav').addClass('bouton-circulaire-fav');
                 });
